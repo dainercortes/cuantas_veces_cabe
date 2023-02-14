632fea717dec50c10216f1d3b753c523f8bddb02
Dainer Cortes <dainer.ramirez2003@gmail.com>
2023-02-14 10:08:57 -0500

    Agregue try catch en la clase Program

--- cuantasVecesCabe/Program.cs
+++ cuantasVecesCabe/Program.cs
@@ -1,9 +1,16 @@
 ﻿using cuantasVecesCabe;
 
-Operacion op = new Operacion();
+try
+{
+    Operacion op = new Operacion();
 
-Console.WriteLine("Por favor ingrese el primer número mayor a 1000 y el segundo menor a 100");
+    Console.WriteLine("Por favor ingrese el primer número mayor a 1000 y el segundo menor a 100");
 
-op.setNum1();
-op.setNum2();
-Console.WriteLine("\nCabe: " + op.Calculo() + " veces");
+    op.setNum1();
+    op.setNum2();
+    Console.WriteLine("\nCabe: " + op.Calculo() + " veces");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
